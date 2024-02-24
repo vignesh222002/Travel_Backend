b28abfe8a4047907bec4c484996c1de6a9209441
vignesh222002 <gtavignesh22042002@gmail.com>
2024-02-24 21:27:37 +0530

    Added All Trip Extra Data

--- src/modules/trip/trip.helper.ts
+++ src/modules/trip/trip.helper.ts
@@ -1,9 +1,53 @@
-import { getAllTripsResponse, getTripByIdResponse, getTripByIdResponseFormatter, places_visited } from "./trip.interfaces";
+import { AllTripApiData, getAllTripsResponse, getTripByIdResponse, getTripByIdResponseFormatter, places_visited } from "./trip.interfaces";
+
+const data = [
+    {
+        "id": 3,
+        "description": "Default description for Trips",
+        "amount_spend": 1000,
+        "members": "Me",
+        "trip_name": "Trip Name",
+        "dates": [
+            "01/01/2024"
+        ],
+        "places": [
+            "Ooty"
+        ],
+        "place_image_link": "https://tamilnadutourisminfo.com/wp-content/uploads/2019/03/ooty_tamilnadu.jpg"
+    },
+    {
+        "id": 6,
+        "description": "Testing Description",
+        "amount_spend": 9000,
+        "members": "Me,Testing,Balu",
+        "trip_name": "Trip Name",
+        "dates": [
+            "19/02/2024",
+            "20/02/2024"
+        ],
+        "places": [
+            "Ooty",
+            "KodaiKanal",
+            "Kolli Hills"
+        ],
+        "place_image_link": "https://tamilnadutourisminfo.com/wp-content/uploads/2019/03/ooty_tamilnadu.jpg"
+    }
+]
 
 export const getAllTripsResponseFormatter = async (data: getAllTripsResponse[]) => {
-    let result: any = []
+    let result: AllTripApiData = {
+        total_trips: 0,
+        total_places_visited: 0,
+        places_visited: [],
+        total_amount_spent: 0,
+        total_days_spent: 0,
+        total_oneday_trips: 0,
+        total_multiday_trips: 0,
+        trip_data: [],
+    }
 
-    data?.map(item => {
+    // Trip Data
+    await data?.map(item => {
         // Add All Dates
         let dates = item.Trip_days_ref.reduce((dates: string[], item) => {
             const date = item.date;
@@ -21,7 +65,7 @@ export const getAllTripsResponseFormatter = async (data: getAllTripsResponse[]) 
 
         // Add Place Image Link too and the Logic for return Priority Place Image
 
-        result.push({
+        result.trip_data.push({
             id: item.id,
             description: item.description,
             amount_spend: item.amount_spend,
@@ -32,6 +76,33 @@ export const getAllTripsResponseFormatter = async (data: getAllTripsResponse[]) 
             place_image_link: item.Trip_days_ref[0].place_ref.image_link
         })
     })
+
+    // Places Visited
+    await result?.trip_data?.map(item => {
+        item?.places?.map(place => {
+            if (!result.places_visited.find(find_place => place === find_place)) {
+                result.places_visited.push(place)
+            }
+        })
+    })
+
+    // Total Amount Spent
+    result.total_amount_spent = await result.trip_data.reduce((total, value) => total + value.amount_spend, 0);
+
+    // Total Days Spent, Total Multi Days Spent, Total One Day Spent
+    await result.trip_data.map(async (item) => {
+        if (item.dates.length) {
+            result.total_days_spent = await item.dates.length + result.total_days_spent
+            if (item.dates.length === 1) result.total_oneday_trips++
+            else result.total_multiday_trips++
+        }
+    })
+
+    // Total Places Visited
+    result.total_places_visited = result.places_visited.length
+
+    // Total Trips
+    result.total_trips = result.trip_data.length
 
     return result;
 }
--- src/modules/trip/trip.interfaces.ts
+++ src/modules/trip/trip.interfaces.ts
@@ -78,3 +78,25 @@ export interface places_visited {
     place_id: number,
     count: number
 }
+
+export interface AllTripApiData {
+    trip_data: AllTripData[];
+    total_trips: number;
+    total_amount_spent: number;
+    total_days_spent: number;
+    total_places_visited: number;
+    places_visited: string[];
+    total_oneday_trips: number;
+    total_multiday_trips: number;
+}
+
+export interface AllTripData {
+    id: number,
+    description: string,
+    amount_spend: number;
+    members: string;
+    trip_name: string;
+    dates: string[],
+    places: string[],
+    place_image_link: string
+}
